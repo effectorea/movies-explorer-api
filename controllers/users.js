fa428a54db3013044625218f0dc5fb937ef7f974
Romanov Eugene <effectorea@yandex.ru>
2022-06-19 01:42:48 +0300

    fixing the issue

--- controllers/users.js
+++ controllers/users.js
@@ -71,7 +71,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         });
-      res.status(201).send({ token, message: 'Вход выполнен успешно' });
+      res.status(201).send({ user, token, message: 'Вход выполнен успешно' });
     })
     .catch(next);
 };
